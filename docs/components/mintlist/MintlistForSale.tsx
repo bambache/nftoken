ec8cf0e3e713bc10eb78e3a5deae9984fc4fccf4
Florin Balate <florin.balate@gmail.com>
2022-07-27 17:21:42 +0200

    added a way to mint more NFTs into a mintlist at once in UI

--- docs/components/mintlist/MintlistForSale.tsx
+++ docs/components/mintlist/MintlistForSale.tsx
@@ -5,7 +5,14 @@ import { useBoolean } from "../../hooks/useBoolean";
 import { LAMPORTS_PER_SOL } from "../../utils/constants";
 import { NftokenTypes } from "../../utils/NftokenTypes";
 import { LuxButton } from "../atoms/LuxButton";
+import { LuxSubmitButton } from "../atoms/LuxButton";
+import { LuxInputField} from "../atoms/LuxInput.tsx"
+import { Form, Formik, useFormikContext } from "formik";
 import { useNetworkContext } from "../atoms/NetworkContext";
+
+type FormData = {
+  totalMintNumber: number;
+};
 
 export const MintlistForSale = ({
   mintlist,
@@ -17,44 +24,79 @@ export const MintlistForSale = ({
   const minting = useBoolean();
   const feeString =
     parseInt(mintlist.price.lamports) / LAMPORTS_PER_SOL + " SOL";
-  const mintNumber = parseInt("5");
+  const maxMint: number = 8;
+
+  const initialValues: FormData = {
+    totalMintNumber: 1,
+  };
 
   return (
     <div className="mt-5">
       <div className="text-xl flex-column flex-center-center gap-3 mb-3 font-weight-medium">
         <div>This Mintlist is now For Sale.</div>
 
-        <div>You can mint {mintNumber} NFTs for {feeString} each</div>
+        <div>You can mint an NFT for {feeString}</div>
       </div>
 
       <div className="flex-center-center">
         {glowDetected ? (
-          <LuxButton
-            label="Mint NFT"
-            disabled={minting.value}
-            onClick={async () => {
+          <div>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={async ({
+              totalMintNumber,
+            }) => {
               minting.setTrue();
 
-              const { address: wallet } = await window.glow!.connect();
-              const { transactionBase64 } = await constructMintNftTx({
-                wallet,
-                network,
-                mintlist,
-                mintNumber,
-              });
+              console.log(totalMintNumber);
 
-              try {
-                await window.glow!.signAndSendTransaction({
-                  transactionBase64,
-                  network,
-                });
-              } catch (err) {
-                console.error(err);
+              while(totalMintNumber > 0) {
+                var mintNumber:number = 0;
+                if (totalMintNumber >= maxMint)
+                  mintNumber = maxMint;
+                else
+                  mintNumber = totalMintNumber%maxMint;
+
+                console.log(mintNumber);
+                if (mintNumber > 0) {
+                  totalMintNumber -= mintNumber;
+
+                  const { address: wallet } = await window.glow!.connect();
+                  const { transactionBase64 } = await constructMintNftTx({
+                      wallet,
+                      network,
+                      mintlist,
+                      mintNumber,
+                      });
+
+                  try {
+                    await window.glow!.signAndSendTransaction({
+                        transactionBase64,
+                        network,
+                        });
+                  } catch (err) {
+                    console.error(err);
+                  }
+                }
               }
 
               minting.setFalse();
             }}
-          />
+          >
+            <Form>
+            <LuxInputField
+              label="Number of NFTs to mint at once"
+              name="totalMintNumber"
+              type="number"
+              required
+            />
+            <LuxSubmitButton
+              label="Mint NFT(s)"
+              disabled={minting.value}
+            />
+            </Form>
+            </Formik>
+          </div>
         ) : (
           <LuxButton
             label={"Download Glow"}
